cfd71e5e53c5a12a20a0266b74795263edbe6547
Hershko25 <hershko25@gmail.com>
2022-02-17 19:31:29 +0200

    Update CardNumber.jsx

--- src/FCC/CardNumber.jsx
+++ src/FCC/CardNumber.jsx
@@ -5,7 +5,7 @@ import '../Css/Cards.css'
 
 
 export default function CardNumber(props){ //Run on arr from Product and send to Cards
-    const letitrun=props.arrvideos.map((per)=><Cards test={per}/>)
+    const letitrun=props.arrvideos.map((per,index)=><Cards test={per} key={index}/>)
     return (
         <div className="divcard" >
         {letitrun}
